0e81caa607d9117e2846212e8708e9c3ff9491e5
Natt Akuma <akumanatt925@gmail.com>
2023-11-27 15:10:32 +0700

    Revert "Increase audio mixing resolution"
    This reverts commit e9034da70b20a09fb469b5b12994138c54f847a0.

--- fpga/source/audio/audio.v
+++ fpga/source/audio/audio.v
@@ -29,11 +29,11 @@ module audio(
     output wire        i2s_data);
 
     wire        next_sample;
-    wire [18:0] psg_left;
-    wire [18:0] psg_right;
+    wire [15:0] psg_left;
+    wire [15:0] psg_right;
 
-    wire [22:0] pcm_left;
-    wire [22:0] pcm_right;
+    wire [15:0] pcm_left;
+    wire [15:0] pcm_right;
 
     //////////////////////////////////////////////////////////////////////////
     // Programmable Sound Generator
@@ -84,13 +84,16 @@ module audio(
     // I2S DAC interface
     //////////////////////////////////////////////////////////////////////////
 
-    wire [23:0] psg_l = {psg_left[18], psg_left, 4'b0};
-    wire [23:0] psg_r = {psg_right[18], psg_right, 4'b0};
-    wire [23:0] pcm_l = {pcm_left[22], pcm_left};
-    wire [23:0] pcm_r = {pcm_right[22], pcm_right};
+    wire [16:0] psg_l = {psg_left[15], psg_left};
+    wire [16:0] psg_r = {psg_right[15], psg_right};
+    wire [16:0] pcm_l = {pcm_left[15], pcm_left};
+    wire [16:0] pcm_r = {pcm_right[15], pcm_right};
 
-    wire [23:0] left_data = psg_l + pcm_l;
-    wire [23:0] right_data = psg_r + pcm_r;
+    wire [16:0] mix_l = psg_l + pcm_l;
+    wire [16:0] mix_r = psg_r + pcm_r;
+
+    wire [23:0] left_data = {mix_l, 7'b0};
+    wire [23:0] right_data = {mix_r, 7'b0};
 
     dacif dacif(
         .rst(rst),
