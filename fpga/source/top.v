cd23de8c5268b945a329c4abe8c91da24a0115cd
jburks <joe@wavicle.org>
2024-03-27 20:30:06 -0700

    Bump version to 0.47.0 in preparation for R47

--- fpga/source/top.v
+++ fpga/source/top.v
@@ -188,7 +188,7 @@ module top(
             case(dc_select_r)
                 6'h0: rddata = dc_hscale_r;
                 6'h1: rddata = dc_active_hstop_r[9:2];
-                default: rddata = 8'h00;
+                default: rddata = 8'd0;
             endcase
         end
         5'h0B: begin
@@ -196,7 +196,7 @@ module top(
                 6'h0: rddata = dc_vscale_r;
                 6'h1: rddata = dc_active_vstart_r[8:1];
                 6'h5: rddata = fx_fill_length_low;
-                default: rddata = 8'h03;
+                default: rddata = 8'd47;
             endcase
         end
         5'h0C: begin
@@ -204,7 +204,7 @@ module top(
                 6'h0: rddata = dc_border_color_r;
                 6'h1: rddata = dc_active_vstop_r[8:1];
                 6'h5: rddata = fx_fill_length_high;
-                default: rddata = 8'h03;
+                default: rddata = 8'd0;
             endcase
         end
 
